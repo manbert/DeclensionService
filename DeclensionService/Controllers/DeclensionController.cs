29699fbc8dbb2806ac318f72812218f47ca6533e
Альберт <mustafin.albert@mail.ru>
2020-09-07 16:00:59 +0500

    удалены неиспользуемые using

--- DeclensionService/Controllers/DeclensionController.cs
+++ DeclensionService/Controllers/DeclensionController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Cyriller;
-using Microsoft.AspNetCore.Http;
+﻿using Cyriller;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DeclensionService.Controllers
